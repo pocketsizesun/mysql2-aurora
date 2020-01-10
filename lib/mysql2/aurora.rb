cfa44bb9cc90795f7d600353c30e2dc6e8ebcf7c
Julien D <julien@unitylab.io>
2020-01-11 00:12:37 +0100

    fix

--- lib/mysql2/aurora.rb
+++ lib/mysql2/aurora.rb
@@ -7,24 +7,26 @@ module Mysql2
   # mysql2 aurora module
   # @note This module patch Mysql2::Client
   module Aurora
+    ORIGINAL_CLIENT_CLASS = ::Mysql2.send(:remove_const, :Client)
+
     # Implement client patch
     class Client
       attr_reader :client
 
       AuroraReadOnlyError = Class.new(StandardError) do
-        attr_reader :innodb_read_only_value
+        attr_reader :read_only_value
 
-        def initialize(innodb_read_only_value)
-          @innodb_read_only_value = innodb_read_only_value
-          super("innodb_read_only_value was '#{innodb_read_only_value}', expected: 'OFF'")
+        def initialize(read_only_value)
+          @read_only_value = read_only_value
+          super("read_only_value was '#{read_only_value}', expected: 'OFF'")
         end
       end
 
       AURORA_READONLY_ERROR = '--read-only'
       AURORA_READONLY_CHECK_QUERY = \
-        "SHOW GLOBAL VARIABLES LIKE 'innodb_read_only';"
+        "SHOW GLOBAL VARIABLES LIKE '%s';"
 
-      AURORA_RECONNECT_ERROR_MESSAGES = [
+      AURORA_CONNECTION_ERRORS = [
         AURORA_READONLY_ERROR,
         'client is not connected',
         'Lost connection to MySQL server',
@@ -37,22 +39,34 @@ module Mysql2
       # @param [Hash] opts Options
       # @option opts [Integer] aurora_max_retry Max retry count, when failover. (Default: 5)
       def initialize(opts)
+        @original_args = opts
         @opts      = Mysql2::Util.key_hash_as_symbols(opts)
         @max_retry = (@opts.delete(:aurora_max_retry) || 5).to_i
         aurora_reconnect!
       end
 
+      def aurora_reconnect!
+        begin
+          @client.close unless @client.nil?
+        rescue => e
+          warn "[mysql2-aurora] reconnect! error: #{e.message} (#{e.class})"
+        end
+        @client = ::Mysql2::Aurora::ORIGINAL_CLIENT_CLASS.new(@original_args)
+      end
+
       # Execute query with reconnect
       # @note [Override] with reconnect.
       def query(*args)
+        aurora_reconnect! if client.closed?
         begin
           client.query(*args)
-        rescue Mysql2::Error => e
-          if aurora_reconnect_error?(e.message)
-            aurora_auto_reconnect!(e)
-          else
-            raise e
+        rescue ::Mysql2::Error => e
+          if aurora_connection_error?(e.message)
+            aurora_wait_for_availability_after(e)
+            aurora_reconnect!
           end
+
+          raise e
         end
       end
 
@@ -60,31 +74,13 @@ module Mysql2
         message.include?(AURORA_READONLY_ERROR)
       end
 
-      def aurora_reconnect_error?(message)
-        AURORA_RECONNECT_ERROR_MESSAGES.any? do |matching_str|
+      def aurora_connection_error?(message)
+        AURORA_CONNECTION_ERRORS.any? do |matching_str|
           message.include?(matching_str)
         end
       end
 
-      # Reconnect to database and Set `@client`
-      # @note If client is not connected, Connect to database.
-      def aurora_reconnect!
-        query_options = {}
-        unless @client.nil?
-          query_options = query_options.merge(@client.query_options || {})
-          begin
-            @client.close
-          rescue StandardError => e
-            warn "[mysql2-aurora] client close error: #{e.message} (#{e.class})"
-            nil
-          end
-        end
-
-        @client = Mysql2::Aurora::ORIGINAL_CLIENT_CLASS.new(@opts)
-        @client.query_options.merge!(query_options)
-      end
-
-      def aurora_auto_reconnect!(error)
+      def aurora_wait_for_availability_after(error)
         warn "[mysql2-aurora] auto reconnect origin error: #{error.message}, max retries: #{@max_retry}"
         try_count = 1
         begin
@@ -92,28 +88,31 @@ module Mysql2
 
           warn "[mysql2-aurora] Retry after #{retry_interval_seconds}seconds"
           sleep retry_interval_seconds
-          aurora_reconnect!
+          check_client = ::Mysql2::Aurora::ORIGINAL_CLIENT_CLASS.new(@opts)
 
-          return unless aurora_readonly_error?(error.message)
+          if aurora_readonly_error?(error.message)
+            # if error was a readonly type, it must check that we are not being
+            # reconnected to a slave, so we ensure that we are connected to a
+            # master node by checking 'innodb_read_only' MySQL's variable
+            # it must be set to 'OFF' if we are on the master
+            show_variables_res = check_client.query(
+              format(
+                AURORA_READONLY_CHECK_QUERY, ::Mysql2::Aurora.read_only_variable
+              ),
+              as: :array
+            ).to_a
 
-          # if error was a readonly type, it must check that we are not being
-          # reconnected to a slave, so we ensure that we are connected to a
-          # master node by checking 'innodb_read_only' MySQL's variable
-          # it must be set to 'OFF' if we are on the master
-          innodb_readonly_result = client.query(
-            AURORA_READONLY_CHECK_QUERY
-          ).to_a.first
-
-          if innodb_readonly_result.nil? ||
-             innodb_readonly_result.fetch('Value', '').to_s.upcase != 'OFF'
-            raise AuroraReadOnlyError
+            if show_variables_res.length == 0 ||
+              show_variables_res[0][1].to_s.upcase != 'OFF'
+              raise AuroraReadOnlyError, show_variables_res[0][1]
+            end
+          else
+            check_client.ping
           end
-        rescue => e
+        rescue ::Mysql2::Error, AuroraReadOnlyError => e
           warn "[mysql2-aurora] auto reconnect error: #{e.message}"
           try_count += 1
-          raise e if try_count > @max_retry
-
-          retry
+          retry if try_count <= @max_retry
         end
 
         warn "[mysql2-aurora] auto-reconnect success"
@@ -124,7 +123,7 @@ module Mysql2
       # @param [Array]  args  Method arguments
       # @param [Proc]   block Method block
       def method_missing(name, *args, &block) # rubocop:disable Style/MethodMissingSuper, Style/MissingRespondToMissing
-        @client.public_send(name, *args, &block)
+        @client.__send__(name, *args, &block)
       end
 
       # Delegate method call to Mysql2::Client.
@@ -132,18 +131,27 @@ module Mysql2
       # @param [Array]  args  Method arguments
       # @param [Proc]   block Method block
       def self.method_missing(name, *args, &block) # rubocop:disable Style/MethodMissingSuper, Style/MissingRespondToMissing
-        Mysql2::Aurora::ORIGINAL_CLIENT_CLASS.public_send(name, *args, &block)
+        ::Mysql2::Aurora::ORIGINAL_CLIENT_CLASS.__send__(
+          name, *args, &block
+        )
       end
 
       # Delegate const reference to class.
       # @param [Symbol] name Const name
       def self.const_missing(name)
-        Mysql2::Aurora::ORIGINAL_CLIENT_CLASS.const_get(name)
+        ::Mysql2::Aurora::ORIGINAL_CLIENT_CLASS.const_get(name)
       end
     end
 
-    # Swap Mysql2::Client
-    ORIGINAL_CLIENT_CLASS = Mysql2.send(:remove_const, :Client)
-    Mysql2.const_set(:Client, Mysql2::Aurora::Client)
+    module_function
+    def read_only_variable=(arg)
+      @read_only_variable = arg.to_s
+    end
+
+    def read_only_variable
+      @read_only_variable ||= 'innodb_read_only'
+    end
   end
 end
+
+Mysql2.const_set(:Client, ::Mysql2::Aurora::Client)
